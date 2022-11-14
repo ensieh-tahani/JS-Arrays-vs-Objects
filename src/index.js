50651b96918d2f813cb0dc193f7af0210dcf5e45
ensieh-tahani <ensieh.tahani@gmail.com>
2022-11-15 00:26:33 +0330

    first commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+let paris = {
+    name: "Paris",
+    country: "France",
+    language: "French",
+    temperature: 18,
+    capitalCity: true
+  };
+  //JS Challenge 1
+  console.log(paris.temperature);
+  //JS Challenge 2
+
+let sydney = {
+    name: "Sydney",
+    country: "Australia",
+    language: "English",
+    temperature: 30,
+    capitalCity: false
+  };
+  console.log(sydney.temperature);
+
+  //JS Challenge 3
+  let australianCities = ["Sydney", "Melbourne", "Brisbane"];
+  console.log(australianCities[0]);
+  console.log(australianCities[1]);
+  console.log(australianCities[2]);
+  //JS Challenge 4
+  let cities = [
+    {
+      name: "Tokyo",
+      country: "Japan",
+      language: "Japanese",
+      temperature: 20,
+      capitalCity: true
+    },
+    {
+      name: "Seoul",
+      country: "South Korea",
+      language: "Korean",
+      temperature: 18,
+      capitalCity: true
+    },
+    {
+      name: "isfahan",
+      country: "Iran",
+      language: "Persian",
+      temperature: 25,
+      capitalCity: false
+    }
+  ];
+  console.log(cities[0].name);
+  console.log(cities[0].temperature);
+  
+  console.log(cities[1].name);
+  console.log(cities[1].temperature);
+  
+  console.log(cities[2].name);
+  console.log(cities[2].temperature);
